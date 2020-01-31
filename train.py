612fe403927ecba9b76705251cf6f89e7a752f0d
Haotao Lai <haotao.lai@gmail.com>
2020-01-30 21:22:55 -0500

    resume best loss filter

--- train.py
+++ train.py
@@ -154,7 +154,7 @@ def train_conditional_model():
     h2, c2 = h2.to(device), c2.to(device)
     h3, c3 = h3.to(device), c3.to(device)
 
-    t_loss, v_loss = [], []
+    t_loss, v_loss, best_val_loss = [], [], 1E10
     start_time = time.time()
     for epoch in range(args.num_epochs):
         ct_loss = 0
@@ -194,31 +194,35 @@ def train_conditional_model():
         logger.info('====> Epoch #{}: Average train loss: {:.4f}'
               .format(epoch + 1, avg_loss))
 
-        # note: disable the validation process, sicne my PC run out of memory
-        # # validation
-        # v_data = list(enumerate(v_loader))[0][1]
-        # for i in range(len(v_data)):
-        #     v_data[i] = v_data[i].to(device)
+        # validation
+        v_data = list(enumerate(v_loader))[0][1]
+        for i in range(len(v_data)):
+            v_data[i] = v_data[i].to(device)
 
-        # strks, strks_m, sents, sents_m, onehots = v_data
+        strks, strks_m, sents, sents_m, onehots = v_data
 
-        # w_prev = onehots.narrow(1, 0, 1)
-        # x = strks.narrow(1, 0, args.timesteps)
-        # strks_m = strks_m.narrow(1, 0, args.timesteps)
-        # target = strks.narrow(1, 1, args.timesteps)
+        w_prev = onehots.narrow(1, 0, 1)
+        x = strks.narrow(1, 0, args.timesteps)
+        strks_m = strks_m.narrow(1, 0, args.timesteps)
+        target = strks.narrow(1, 1, args.timesteps)
 
-        # output, _ = model(x, strks_m, onehots, sents_m, w_prev, k_prev,
-        #                 (h1, c1), (h2, c2), (h3, c2))
-        # tmp_loss = neg_log_likelihood(output, target, strks_m)
-        # v_loss.append(tmp_loss)
-        # print('====> Epoch: {} Average validation loss: {:.4f}'.format(
-        #     epoch + 1, tmp_loss))
+        output, _ = model(x, strks_m, onehots, sents_m, w_prev, k_prev,
+                        (h1, c1), (h2, c2), (h3, c2))
+        val_loss = neg_log_likelihood(output, target, strks_m)
+        v_loss.append(val_loss)
+        print('====> Epoch: {} Average validation loss: {:.4f}'.format(
+            epoch + 1, val_loss))
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            save_checkpoint(epoch, model, val_loss, optimizer, args.model_dir, args.task + '_best.pt')
 
         # save the intermediate result
-        tmp_loss = 1.0
         filename = args.task + '_epoch_{}.pt'.format(epoch + 1)
-        save_checkpoint(epoch, model, tmp_loss, optimizer,
+        save_checkpoint(epoch, model, val_loss, optimizer,
                         args.model_dir, filename)
+
+
         logger.info('wall time: {}s'.format(time.time() - start_time))
 
     # visualize the change of the loss
